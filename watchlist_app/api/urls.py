00d42797a2800c95ea01a553f55f96f0374c2c3b
Ailing <irene024081@gmail.com>
2022-12-02 10:21:31 +0900

    tried mixins to create view

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from watchlist_app.api.views import WatchListAV, WatchListDetailAV, StreamPlatformAV, StreamPlatformDetailsAV
+from watchlist_app.api.views import ReviewDetail,ReviewList,WatchListAV, WatchListDetailAV, StreamPlatformAV, StreamPlatformDetailsAV
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name = 'watch_list'),
     path('<int:pk>', WatchListDetailAV.as_view(), name = 'movie_details'),
     path('stream/', StreamPlatformAV.as_view(), name = 'stream'),
     path('stream/<int:pk>', StreamPlatformDetailsAV.as_view(), name = 'stream_details'),
+    path('review', ReviewList.as_view(), name = 'review_list'),
+    path('review/<int:pk>', ReviewDetail.as_view(), name='review_details'),
 ]
